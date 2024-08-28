3d49ba87590c0d411ac75ea3d00aa36578cc7dbc
nimon <nimon@42paris.fr>
2024-08-28 15:30:14 +0200

    Fix tps issue

--- commands/interacte/tps.js
+++ commands/interacte/tps.js
@@ -8,18 +8,18 @@ module.exports = {
         .setDescription('Get the TPS of the Minecraft server.'),
     async execute(interaction) {
         await interaction.deferReply();
-        var tps = [];
+        tps_list = [];
         bots.forEach((bot) => {
             bot.once('chat:tps', (tps) => {
-                tps.push([bot.username, tps]);
+                tps_list.push([bot.username, tps]);
             });
             bot.chat('/tps');
         });
         await wait(5000);
-        tps.sort((a, b) => b[1] - a[1]);
-        tps.forEach((tp) => {
-            tp[0] = config.servers.find((server) => server.username === tp[0]).name;
+        tps_list.sort((a, b) => b[1] - a[1]);
+        tps_list.forEach((tps) => {
+            tps[0] = config.servers.find((server) => server.username === tps[0]).name;
         });
-        return interaction.followUp(tps.map((tp) => `- \`${tp[0]}\`: ${tp[1]}%`).join('\n'));
+        return interaction.followUp(tps_list.map((tps) => `- \`${tps[0]}\`: ${tps[1]}%`).join('\n'));
     },
 };
